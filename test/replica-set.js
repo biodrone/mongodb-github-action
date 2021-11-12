ba445b8e8b916cdd6bdff02612378fb67e7026f7
Josh Jacobs <josh@joshjacobs.net>
2021-11-12 18:34:16 +0000

    test: back to connectionstring

--- test/replica-set.js
+++ test/replica-set.js
@@ -4,13 +4,13 @@ const Lab = require('@hapi/lab')
 const Mongoose = require('mongoose')
 const { expect } = require('@hapi/code')
 
-const { MONGODB_PORT = 27017, MONGODB_REPLICA_SET = 'mongodb-test-rs' } = process.env
+const { MONGODB_PORT = 27017, MONGODB_REPLICA_SET = 'mongodb-test-rs', MONGODB_DB = 'ci', MONGODB_USERNAME = 'ci', MONGODB_PASWORD = 'ci' } = process.env
 
 const { describe, it, before, after } = (exports.lab = Lab.script())
 
 describe('MongoDB Replica Set ->', () => {
   before(async () => {
-    const connectionString = `mongodb://localhost:${MONGODB_PORT}/test?replicaSet=${MONGODB_REPLICA_SET}`
+    const connectionString = `mongodb://${MONGODB_USERNAME}:${MONGODB_PASWORD}localhost:${MONGODB_PORT}/${MONGODB_DB}?replicaSet=${MONGODB_REPLICA_SET}`
 
     console.log('---------------------------------------------------------------------')
     console.log('connecting to MongoDB using connection string -> ' + connectionString)
@@ -20,9 +20,6 @@ describe('MongoDB Replica Set ->', () => {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       serverSelectionTimeoutMS: 1500,
-      user: 'ci',
-      pass: 'ci',
-      dbName: 'ci',
       authSource: 'admin'
     })
   })
